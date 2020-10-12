7e0c2825a217d562f621cee872190e83fde97aa2
Jose Cruz <josec300@live.com>
2020-10-12 12:27:26 -0400

    added a entitiy pojo class

---
+++ Project-Management/src/main/java/com/app/ProjectManagement/Entities/Project.java
@@ -0,0 +1,57 @@
+package com.app.ProjectManagement.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long projectId;
+    private String name;
+    private String stage; // Not started, completed, in progress
+    private String description;
+
+    public Project() {  //default constructor
+    }
+
+    public Project(String name, String stage, String description) {
+        this.name = name;
+        this.stage = stage;
+        this.description = description;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
