384ff06386cf0bf340266b44eb6b48a8c03e5d68
Jose Cruz <josec300@live.com>
2020-10-16 11:43:01 -0400

    Made a employee repository

---
+++ Project-Management/src/main/java/com/app/ProjectManagement/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.app.ProjectManagement.dao;
+
+import com.app.ProjectManagement.Entities.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee,Long> {
+    
+}
