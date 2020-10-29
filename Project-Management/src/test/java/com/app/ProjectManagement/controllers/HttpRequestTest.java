2d98f468387fca2dde187b8d09b902695abe011d
Jose Cruz <josec300@live.com>
2020-10-29 12:47:18 -0400

    changed some files before and forgot to update it lol

--- Project-Management/src/test/java/com/app/ProjectManagement/controllers/HttpRequestTest.java
+++ Project-Management/src/test/java/com/app/ProjectManagement/controllers/HttpRequestTest.java
@@ -1,4 +1,28 @@
 package com.app.ProjectManagement.controllers;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
+    @LocalServerPort
+    private  int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void homePageReturnVersionNumberCorrectly_thenSuccess(){
+        String renderHtml= this.restTemplate.getForObject("http://localhost:"+port+"/",String.class);
+        assertEquals(renderHtml.contains("3.0.0"),true);
+    }
+
 }
