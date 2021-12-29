395c8414733f456cc3831daca02e343f0935135e
jruz9 <josec300@live.com>
2021-12-29 14:11:18 -0500

    worked on using authorization in memory

--- Project-Management/src/main/java/com/app/ProjectManagement/security/SecurityConfiguration.java
+++ Project-Management/src/main/java/com/app/ProjectManagement/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.app.ProjectManagement.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,14 +18,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth)throws Exception{
         auth.inMemoryAuthentication()
                 .withUser("myuser")
-                .password("pass")
-                .roles("USER");
+                    .password("pass")
+                    .roles("USER")
+                .and()
+                .withUser("managerUser")
+                    .password("pass3")
+                    .roles("ADMIN");
 
 
     }
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance(); //depercated that why it is striking it lol just for learning
+    }
+    protected void  configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/projects/new").hasRole("Admin")
+                .antMatchers("/employee/new").hasRole("Admin")
+                .antMatchers("/").authenticated().and().formLogin();
     }
 
 }
